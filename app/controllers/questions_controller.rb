3c3a395dc09a3e041f306e0998dce26dc148f7df
Juan Garcia <juagarca@gmail.com>
2020-11-09 14:58:12 +0000

    visula improvements added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
     if your_message == ''
       "Please ask me a question"
     elsif (your_message == 'I am going to work right now!') || (your_message == 'I AM GOING TO WORK RIGHT NOW!')
-      'Well done!'
+      'Great!'
     elsif your_message.slice(-1, 1) == '?'
       'Silly question, get dressed and go to work!'
     else
